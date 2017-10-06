b228bd0108be6b7a25601a3d62b9f4b7aa158308
18ariana <32478103+18ariana@users.noreply.github.com>
2017-10-06 18:12:18 +0300

    Add files via upload

---
+++ caesar (1).py
@@ -0,0 +1,42 @@
+def encrypt_caesar(plaintext, shift):
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("")
+    ''
+    """
+    ciphertext = ""
+    for i in range(0, len(plaintext)):
+        if (ord(plaintext[i]) + shift > 63) and (ord(plaintext[i]) + shift < 91):
+            ciphertext += chr(ord(plaintext[i]) + shift)
+        elif (ord(plaintext[i]) + shift > 96) and (ord(plaintext[i]) + shift < 123):
+            ciphertext += chr(ord(plaintext[i]) + shift)
+        elif (ord(ciphertext[i]) + shift < 64) or (ord(ciphertext[i]) + shift > 91) or (ord(ciphertext[i]) + shift < 96) or (ord(ciphertext[i]) + shift > 122):
+            plaintext += chr(ord(ciphertext[i]) + shift - 26)
+        else:
+            ciphertext += plaintext[i]
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext, shift):
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("")
+    ''
+    """
+    plaintext = ""
+    for i in range(0, len(ciphertext)):
+        if ((ord(ciphertext[i]) - shift > 63) and (ord(ciphertext[i]) - shift < 91)):
+            plaintext += chr(ord(ciphertext[i]) - shift)
+        elif ((ord(ciphertext[i]) - shift > 96) and (ord(ciphertext[i]) - shift < 123)):
+            plaintext += chr(ord(ciphertext[i]) - shift)
+        elif (ord(ciphertext[i]) - shift < 64) or (ord(ciphertext[i]) - shift > 91) or (ord(ciphertext[i]) - shift < 96) or (ord(ciphertext[i]) - shift > 122):
+            plaintext += chr(ord(ciphertext[i]) - shift + 26)
+        else:
+            ciphertext += plaintext[i]
+    return plaintext
