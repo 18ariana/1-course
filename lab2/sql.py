50877fc55fc6a530bdadd2bf27deef1f3d316977
18ariana <32478103+18ariana@users.noreply.github.com>
2018-04-11 11:05:36 +0300

    Update sql.py

--- lab2/sql.py
+++ lab2/sql.py
@@ -22,9 +22,9 @@ print(records)
 """ 1.Сколько мужчин и женщин представлено в этом наборе данных? """
 cursor.execute(
    """
-   SELECT gender, AVG(height), COUNT(gender) AS ammount
-   FROM mlboot
-   GROUP BY gender
+   select gender, AVG(height), COUNT(gender) AS ammount
+   from mytable
+   group by gender
    """
    )
 print('Задание 1')
@@ -35,8 +35,8 @@ print(tabulate(fetch_all(cursor), "keys", "psql"))
 
 cursor.execute(
     """
-    select count(alco) from mytable where gender = '1' and alco = '1';
-    select count(alco) from mytable where gender = '2' and alco = '1';  
+    select count(alco) from mytable where gender = '1' and alco = '1'
+    select count(alco) from mytable where gender = '2' and alco = '1'  
     """
 )
 
@@ -63,11 +63,11 @@ print(tabulate(fetch_all(cursor), "keys", "psql"))
     
 cursor.execute(
     """
-    SELECT DISTINCT ABS(
-        (SELECT median(age) / 30 FROM mlboot WHERE smoke='1') - \
-        (SELECT median(age) / 30 FROM mlboot WHERE smoke='0')
-    )::int AS diff
-    FROM mlboot
+    select distinct abs(
+        (select median(age) / 30 from mytable where smoke='1') - \
+        (select median(age) / 30 from mytable where smoke='0')
+    )::int as difference
+    from mytable
     """
 ) 
 print('Задание 4')
@@ -75,23 +75,109 @@ print(tabulate(fetch_all(cursor), "keys", "psql"))
 
 
 
+""" 5.  1. Создайте новый признак age_years – возраст в годах, округлив до целых (round).
+       Для данного примера отберите курящих мужчин от 60 до 64 лет включительно.
+        2. Категории уровня холестрина на рисунке и в наших данных отличаются. Отображение значений на картинке
+       в значения признака cholesterol следующее: 4 ммоль/л -> 1, 5-7 ммоль/л -> 2, 8 ммоль/л -> 3.
+        3. Интересуют 2 подвыборки курящих мужчин возраста от 60 до 64 лет включительно:
+       первая с верхним артериальным давлением строго меньше 120 мм рт.ст. и концентрацией холестерина – 4 ммоль/л,
+       а вторая – с верхним артериальным давлением от 160 (включительно) до 180 мм рт.ст. (не включительно)
+       и концентрацией холестерина – 8 ммоль/л.
+       Во сколько раз (округленно, round) отличаются доли больных людей (согласно целевому признаку, cardio)
+       в этих двух подвыборках?
+       """
+
+cursor.execute(
+    """
+    select(select avg(cardio) from mytable where gender = '2' and age >= 60365 and age <= 64365 and smoke = '1' and ap_hi >= 160 and 
+    ap_hi < 180 and cholesterol = '1') / (select avg(cardio) from mytable where gender = '2' and age >= 21900 and age <= 64*365
+    and smoke = '1' and ap_hi < 120 and cholesterol = '1') from mytable limit 1
+    """
+)
+print('Задание 5')
+print(tabulate(fetch_all(cursor), "keys", "psql"))
+
+
+""" 6. Постройте новый признак – BMI (Body Mass Index). Для этого надо вес в килограммах
+    поделить на квадрат роста в метрах. Нормальными считаются значения BMI от 18.5 до 25.
+    Выбрать верные утверждения:
+    1. Медианный BMI по выборке превышает норму.
+    2. У женщин в среднем BMI ниже, чем у мужчин.
+    3. У здоровых в среднем BMI выше, чем у больных.
+    4. В сегменте здоровых и непьющих мужчин в среднем BMI ближе к норме,
+       чем в сегменте здоровых и непьющих женщин.
 """
+cursor.execute(
+    """
+   select median(weight / (height / 100) ^ 2) as m_BMI from mytable
+    """
+)
+print('Задание 6.1')
+print(tabulate(fetch_all(cursor), "keys", "psql"))
 
- 
-    
-    
+cursor.execute(
+    """
+    select (select avg(weight) from mytable where gender = '1' )/(select avg(pow(height/100,2)) from mytable where gender = '1' ) 
+    from mytable limit 1 
+    select (select avg(weight) from mytable where gender = '2' )/(select avg(pow(height/100,2)) from mytable where gender = '2' )
+    from mytable limit 1 
+    """
+)
+print('Задание 6.2')
+print(tabulate(fetch_all(cursor), "keys", "psql"))  
+
+cursor.execute(
+    """
+    select (select avg(weight) from mytable where cardio = '1' )/(select avg(pow(height/100,2)) from mytable where cardio = '1' ) 
+    from mytable limit 1 
+    select (select avg(weight) from mytable where cardio = '0' )/(select avg(pow(height/100,2)) from mytable where cardio = '0' ) 
+    from mytable limit 1 
+    """
+)
+print('Задание 6.3')
+print(tabulate(fetch_all(cursor), "keys", "psql"))  
+
+cursor.execute(
+    """
+    select (select avg(weight) from mytable where cardio = '0' and gender = '2' and alco = '0' )/(select avg(pow(height/100,2)) 
+    from mytable where cardio = '0' and gender = '2' and alco = '0' ) from mytable limit 1 
+    select (select avg(weight) from mytable where cardio = '0' and gender = '1' and alco = '0' )/(select avg(pow(height/100,2)) 
+    from mytable
+    where cardio = '0' and gender = '1' and alco = '0' ) from mytable limit 1 
+    """
+)
+print('Задание 6.4')
+print(tabulate(fetch_all(cursor), "keys", "psql"))  
+
+"""" 7. Отфильтруйте следующие сегменты пациентов (считаем это ошибками в данных):
+    1. Указанное нижнее значение артериального давления строго выше верхнего.
+    2. Рост строго меньше 2.5%-перцентили или строго больше 97.5%-перцентили.
+       (используйте pd.Series.quantile, если не знаете, что это такое – прочитайте)
+    3. Вес строго меньше 2.5%-перцентили или строго больше 97.5%-перцентили.
+    Сколько процентов данных (округленно, round) мы выбросили? """
+
+cursor.execute(
+    """
+    select count(height) as all, PERCENTILE_CONT(0.025) within group (ORDER BY height) as h_25,
+    PERCENTILE_CONT(0.975) within group (ORDER BY height) as h_975, PERCENTILE_CONT(0.025) within group (ORDER BY weight) as w_25,
+    PERCENTILE_CONT(0.975) within group (ORDER BY weight) as w_975
+    from mytable limit 1 
+    """"
+)
+print('Задание 7, квантили')
+print(tabulate(fetch_all(cursor), "keys", "psql"))
+
+""" 150 <= Рост <=180
+    51 <= Вес <= 108
+    """
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-    
+cursor.execute(
+    """
+    select distinct ( 100 - ((select distinct count(*) * 100 from mytable where ap_hi >= ap_lo and height >= 150 
+    and height <= 180 and weight >= 51 AND weight <= 108) / (select count(*) from mytable ))) as answer from mytable
+    """
+ )
+print('Задание 7, ответ')
+print(tabulate(fetch_all(cursor), "keys", "psql"))  
