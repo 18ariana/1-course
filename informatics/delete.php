e39221cf1162a9931ab2fd3ecef647a9de521b69
18ariana <32478103+18ariana@users.noreply.github.com>
2018-05-30 20:26:34 +0300

    Update delete.php

--- informatics/delete.php
+++ informatics/delete.php
@@ -1,9 +1,15 @@
 <?php
-// delete records
+
 include "connection.php";
-$id_abiturient = $_GET['id_abiturient'];
-$query = "DELETE FROM abit
-	WHERE id_abiturient ='" . $id_abiturient . "'";
+
+$link = mysqli_connect($host, $user, $password, $database) 
+    or die ("Ошибка подключения к базе данных" . mysqli_error());
+
+$id_application = $_GET['id_application'];
+
+$query = "DELETE FROM registration
+	WHERE id_application ='" . $id_application . "'";
+
 $result = mysqli_query($link, $query);
-// redirect
+
 header('location: ./list.php');
